178f288c56051e00612a8eaa415175943a2e4620
jaimeloliveiralima@gmail.com <jaimeloliveiralima@gmail.com>
2021-09-19 20:13:43 -0300

    Ajustes REST

--- src/main/java/com/jaimel/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/jaimel/cursomc/resources/CategoriaResource.java
@@ -3,11 +3,15 @@ package com.jaimel.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jaimel.cursomc.domain.Categoria;
+import com.jaimel.cursomc.services.CategoriaService;
 
 
 @RestController //anotação para controlador REST
@@ -15,16 +19,12 @@ import com.jaimel.cursomc.domain.Categoria;
 
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Categoria");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
